2c65a7f9fdad37df533f7fd3bdfd845d970e2133
Dhiraj <dheerazsah31@gmail.com>
2025-03-18 11:46:04 +0545

    Exact match routes

--- dhiraj-blog/src/App.js
+++ dhiraj-blog/src/App.js
@@ -1,20 +1,20 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
-  const title = 'Welcome ot the new blog';
-  const likes = 50;
-  const link = "http://wwww.google.com"
-
   return (
     <Router>
     <div className="App">
       <Navbar/>
       <div className="content">
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
           </Route>
         </Switch>
       </div>
