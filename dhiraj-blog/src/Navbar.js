19350dcf24d9d498894e9bd1a2cc2f3b36b179f7
Dhiraj <dheerazsah31@gmail.com>
2025-03-18 11:52:55 +0545

    Router links

--- dhiraj-blog/src/Navbar.js
+++ dhiraj-blog/src/Navbar.js
@@ -1,15 +1,12 @@
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Dhiraj Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-                {/* <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "#1169c4",
-                    borderRadius: "8px"
-                }}>New Blog</a> */}
+
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
      );
