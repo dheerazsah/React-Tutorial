3fa96af75d5f25f64a5397febf18d1c15aeb308c
Dhiraj <dheerazsah31@gmail.com>
2025-03-18 11:30:48 +0545

    Using react router

--- dhiraj-blog/src/App.js
+++ dhiraj-blog/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   const title = 'Welcome ot the new blog';
@@ -7,12 +8,18 @@ function App() {
   const link = "http://wwww.google.com"
 
   return (
+    <Router>
     <div className="App">
       <Navbar/>
       <div className="content">
-        <Home/>
+        <Switch>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
